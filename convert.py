591d075c42c90f3537668d52c3b755450bc6b94c
wooriel <wooriel@gmail.com>
2024-11-10 18:34:38 +0900

    Update convert.py

--- convert.py
+++ convert.py
@@ -7,4 +7,5 @@ with open("environment.yml") as file_handle:
 for dependency in environment_data["dependencies"]:
     if isinstance(dependency, dict):
         for lib in dependency['pip']:
-            os.system(f"pip3 install {lib}")  # pip -> pip3
+            os.system(f"pip3.8 install {lib}")  # pip -> pip3
+            # os.system(f"pip3 install {lib}")  # pip -> pip3
