99774e25286f84f323878d53c05d012db9276ef9
riella <wooriel@gmail.com>
2024-12-02 22:32:24 +0900

    remove 3.8 in convert.py and starlab_test.sh, removed installing torch part in environment.yml

--- starlab_test.sh
+++ starlab_test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 echo "<<3D Shape Correspondence Test Start>>"
 
-python3.8 functional_correspondence/functional_correspondence.py --test_dataset=faust --input_feature=hks --load_model=functional_correspondence/pretrained_models/starlab_hks.pth --evaluate
-python3.8 functional_correspondence/find_model_correspondence.py --evaluate --test_dataset=psb --input_features=hks --load_model=functional_correspondence/pretrained_models/starlab_hks.pth --top_k=2
+python functional_correspondence/functional_correspondence.py --test_dataset=faust --input_feature=hks --load_model=functional_correspondence/pretrained_models/starlab_hks.pth --evaluate
+python functional_correspondence/find_model_correspondence.py --evaluate --test_dataset=psb --input_features=hks --load_model=functional_correspondence/pretrained_models/starlab_hks.pth --top_k=2
