93c1649474014fc64ab92d1b4b4bb2c8da4f146c
Hyeonguk Ryu <ryu@hyeonguk.com>
2024-12-23 22:06:44 +0900

    호환성 향상

--- starlab_test.sh
+++ starlab_test.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 echo "<<3D Shape Correspondence Test Start>>"
 
 python functional_correspondence/functional_correspondence.py --test_dataset=faust --input_feature=hks --load_model=functional_correspondence/pretrained_models/starlab_hks.pth --evaluate
